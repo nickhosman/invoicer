b24e16feaab2d6ae5ab468fa3b63e804a2be3fe3
Nick Hosman <nickhosman@gmail.com>
2024-05-23 13:47:32 -0700

    Invoice component

--- pb_app/src/Invoice.jsx
+++ pb_app/src/Invoice.jsx
@@ -1,14 +1,28 @@
-function createInvoice(id, client, business, ...items) {
-    return pb.collection("invoices").create({
-        client,
-        items,
-        business,
-    });
-}
+import { useForm } from "react-hook-form";
 
-function createItem(name, price) {
-    return pb.collection("items").create({
-        name,
-        price,
-    });
+export default function Invoice() {
+    function createInvoice(id, client, business, ...items) {
+        return pb.collection("invoices").create({
+            client,
+            items,
+            business,
+        });
+    }
+
+    function createItem(name, price) {
+        return pb.collection("items").create({
+            name,
+            price,
+        });
+    }
+
+    return (
+        <>
+            <h1>Invoice</h1>
+            <form>
+                <input type="text" placeholder="Client name"/>
+                <input type="text" placeholder="Business name"/>
+            </form>
+        </>
+    )
 }
