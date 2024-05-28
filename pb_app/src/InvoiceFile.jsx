b9b4a525e0048c9c82a942998e9334e4594a0e9b
Nick Hosman <nickhosman@gmail.com>
2024-05-28 12:59:39 -0700

    invoice template v1

--- pb_app/src/InvoiceFile.jsx
+++ pb_app/src/InvoiceFile.jsx
@@ -21,7 +21,7 @@ export default function InvoiceFile() {
             </div>
 
             <div className="grid grid-rows-6 gap-4">
-                <div className="grid grid-cols-6">
+                <div className="grid grid-cols-6 justify-items-center">
                     <p>Quantity</p>
                     <p className="col-span-3">Description</p>
                     <p>Unit Price</p>
@@ -29,17 +29,17 @@ export default function InvoiceFile() {
                 </div>
 
                 {invoice.items.map((item) => (
-                    <div className="grid grid-cols-6 justify-center">
-                        <p className="w-full">{item.quantity}</p>
+                    <div className="grid grid-cols-6 justify-items-center">
+                        <p>{item.quantity}</p>
                         <p className="col-span-3">{item.name}</p>
                         <p>${item.price}</p>
                         <p>${item.price * item.quantity}</p>
                     </div>
                 ))}
 
-                <div className="grid grid-cols-6">
+                <div className="grid grid-cols-6 justify-items-center font-bold">
                     <div></div>
-                    <p className="col-span-3 font-bold justify-self-end">Total</p>
+                    <p className="col-span-3 justify-self-end">Total</p>
                     <div></div>
                     <p>${calculateTotal(invoice.items)}</p>
                 </div>
