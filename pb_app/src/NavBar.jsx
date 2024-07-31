8e667a1847df949c877bd71e89b42d1ec683e09f
Nick <nickhosman@gmail.com>
2024-07-30 22:53:21 -0700

    navbar indicator

--- pb_app/src/NavBar.jsx
+++ pb_app/src/NavBar.jsx
@@ -1,12 +1,21 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function NavBar() {
+    const location = useLocation();
+    const { pathname } = location;
+
+    useEffect(() => {
+        console.log("AAAAAAAAAAAAAAAAAAAA")
+        console.log(pathname);
+    }, [pathname]);
+
     return (
-        <nav className="flex justify-between items-center h-16 bg-cyan-950 text-black relative shadow-sm font-mono text-white" role="navigation">
-            <Link to="/" className="pl-8">Home</Link>
-            <Link to="/profile" className="p-4">Profile</Link>
-            <Link to="/invoice" className="p-4">Invoice</Link>
-            <Link to="/invoice/create" className="p-4">Create Invoice</Link>
+        <nav className="flex justify-between items-center h-16 bg-cyan-950 relative shadow-sm font-mono text-white" role="navigation">
+            <Link to="/" className={pathname == "/" ? "p-8 underline font-bold" : "p-8"}>Home</Link>
+            <Link to="/profile" className={pathname == "/profile" ? "p-8 underline font-bold" : "p-8"}>Profile</Link>
+            <Link to="/invoice" className={pathname == "/invoice" ? "pl-8 underline font-bold" : "pl-8"}>Invoice</Link>
+            <Link to="/invoice/create" className={pathname == "/invoice/create" ? "pl-8 underline font-bold" : "pl-8"}>Create Invoice</Link>
         </nav>
     )
 }
