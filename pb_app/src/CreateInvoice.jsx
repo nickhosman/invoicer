eb0d4b74ff19379e8b4744a1db859caebe014055
Nick <nickhosman@gmail.com>
2024-08-23 22:56:42 -0700

    invoice preview

--- pb_app/src/CreateInvoice.jsx
+++ pb_app/src/CreateInvoice.jsx
@@ -14,6 +14,41 @@ export default function CreateInvoice() {
                 </div>
                 <button>Create Invoice</button>
             </form>
+            <div>
+                <h2>Preview</h2>
+                <div>
+                    <p>Client:</p>
+                    <p>Business:</p>
+                </div>
+                <div>
+                    <p>Quantity</p>
+                    <p>Description</p>
+                    <p>Unit Price</p>
+                    <p>Total</p>
+                </div>
+                <div>
+                    <p>2</p>
+                    <p>Product 1</p>
+                    <p>$10</p>
+                    <p>$20</p>
+                </div>
+                <div>
+                    <p>1</p>
+                    <p>Product 2</p>
+                    <p>$20</p>
+                    <p>$20</p>
+                </div>
+                <div>
+                    <p>3</p>
+                    <p>Product 3</p>
+                    <p>$15</p>
+                    <p>$45</p>
+                </div>
+                <div>
+                    <p>Total</p>
+                    <p>$85</p>
+                </div>
+            </div>
         </div>
     )
 }
