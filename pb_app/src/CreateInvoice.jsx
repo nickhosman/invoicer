043eab6989e295b2a50e80b301a84656b07b4718
Nick <nickhosman@gmail.com>
2024-08-23 22:55:46 -0700

    form skeleton

--- pb_app/src/CreateInvoice.jsx
+++ pb_app/src/CreateInvoice.jsx
@@ -1,7 +1,19 @@
 export default function CreateInvoice() {
     return (
         <div>
-            
+            <h1>Create Invoice</h1>
+            <form>
+                <input type="text" placeholder="Client name" />
+                <input type="text" placeholder="Business name" />
+                <div>
+                    <h2>Items</h2>
+                    <input type="number" placeholder="Quantity" />
+                    <input type="text" placeholder="Description" />
+                    <input type="number" placeholder="Price" />
+                    <button>Add Item</button>
+                </div>
+                <button>Create Invoice</button>
+            </form>
         </div>
     )
 }
