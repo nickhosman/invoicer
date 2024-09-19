2e5687ee79077356abf0b8410e57b89ebb91ed59
Nick <nickhosman@gmail.com>
2024-09-18 21:54:00 -0700

    expanding test page

--- pb_app/src/TestPage.jsx
+++ pb_app/src/TestPage.jsx
@@ -3,6 +3,14 @@ export default function TestPage() {
         <div>
             <h1>Test Page</h1>
             <p>This is a test page.</p>
+            <div>
+                <h2>Test Section</h2>
+                <p>This is a test section.</p>
+            </div>
+            <div>
+                <h2>Test Section #2</h2>
+                <p>This is another test section.</p>
+            </div>
         </div>
     )
 };
