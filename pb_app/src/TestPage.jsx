22fa7d71f5d561f2b8fdef071c614376e8c24726
Nick <nickhosman@gmail.com>
2024-09-24 23:20:55 -0700

    button style tests

--- pb_app/src/TestPage.jsx
+++ pb_app/src/TestPage.jsx
@@ -1,7 +1,12 @@
 export default function TestPage() {
     return (
         <div>
-            
+            <span>Test Page</span>
+            <div>
+                <button>
+                    Test
+                </button>
+            </div>
         </div>
     )
 };
