279ae069b24f43574730da8be165503cb6e10930
Nick <nickhosman@gmail.com>
2024-09-24 23:20:05 -0700

    messing with test pages

--- pb_app/src/TestPage.jsx
+++ pb_app/src/TestPage.jsx
@@ -1,16 +1,7 @@
 export default function TestPage() {
     return (
         <div>
-            <h1>Test Page</h1>
-            <p>This is a test page.</p>
-            <div>
-                <h2>Test Section</h2>
-                <p>This is a test section.</p>
-            </div>
-            <div>
-                <h2>Test Section #2</h2>
-                <p>This is another test section.</p>
-            </div>
+            
         </div>
     )
 };
