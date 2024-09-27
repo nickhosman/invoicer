c209f1e8dfa7832c01356ddb156857e62e7c35f3
Nick <nickhosman@gmail.com>
2024-09-26 22:36:32 -0700

    conditional rendering of create invoice modal

--- pb_app/src/MyInvoices.jsx
+++ pb_app/src/MyInvoices.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import InvoicePreview from "./InvoicePreview"
 
 export default function MyInvoices({ invoices }) {
+    const [ creating, setCreating ] = useState(false);
+
     return (
         <div>
             <span>My Invoices</span>
             <div>
-                <button>
-                    Create New Invoice
-                </button>
+                {creating ?
+                    <button>
+                        Create New Invoice
+                    </button>
+                : null}
             </div>
             <div>
                 {invoices.map((invoice) => (
